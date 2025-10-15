47e0c70ed18a84e75ddd531d353516f3eb97934c
Dieter <dieter.vanderelst@fastmail.net>
2025-10-15 17:02:07 -0400

    added example

--- script_test_lorex.py
+++ script_test_lorex.py
@@ -7,7 +7,6 @@ os.environ.setdefault("OPENCV_VIDEOIO_PRIORITY_GSTREAMER", "0")  # prefer FFmpeg
 cv.ocl.setUseOpenCL(False)
 cv.setNumThreads(1)
 
-
 cam = Lorex.LorexCamera("tiger")
 # 1) Load the saved homography/pose bundle (from your earlier script)
 cam.load_board_bundle()
@@ -23,10 +22,10 @@ u, v = w//2, h//2
 # 2) H-path (raw -> board)
 xH, yH = cam.pixel_to_board_xy(u, v, use_raw=True)
 
-# 3) Ray-plane path using the *same* raw pixel and the *same* K/dist from bundle
-d_cam = hg.pixel_to_ray_cam(u, v, Kb, dist)
-P     = hg.intersect_ray_with_board(d_cam, cam.bundle["R"], cam.bundle["t"])
-xRT, yRT = float(P[0]), float(P[1])
-
-print(f"RAW pixel ({u},{v})  H:({xH:.2f},{yH:.2f})  RT:({xRT:.2f},{yRT:.2f})  Δ=({xRT-xH:.2f},{yRT-yH:.2f}) mm")
-cam.stop()
+# # 3) Ray-plane path using the *same* raw pixel and the *same* K/dist from bundle
+# d_cam = hg.pixel_to_ray_cam(u, v, Kb, dist)
+# P     = hg.intersect_ray_with_board(d_cam, cam.bundle["R"], cam.bundle["t"])
+# xRT, yRT = float(P[0]), float(P[1])
+#
+# print(f"RAW pixel ({u},{v})  H:({xH:.2f},{yH:.2f})  RT:({xRT:.2f},{yRT:.2f})  Δ=({xRT-xH:.2f},{yRT-yH:.2f}) mm")
+# cam.stop()
